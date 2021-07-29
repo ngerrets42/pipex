0bd6e957985bb1b48a6a26ba0ce314e5ed1633c8
Nick <ngerrets@Toaster-Mk-1.localdomain>
2021-07-29 16:25:39 +0200

    remove unnecessary files.

--- src/error.c
+++ src/error.c
@@ -6,13 +6,12 @@
 /*   By: ngerrets <ngerrets@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/07/28 16:30:40 by ngerrets      #+#    #+#                 */
-/*   Updated: 2021/07/29 16:02:07 by ngerrets      ########   odam.nl         */
+/*   Updated: 2021/07/29 16:23:51 by ngerrets      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include "str.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 void	throw_error(const char *err)
