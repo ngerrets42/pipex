a5c5dc7eecd1ed5fabd4523f9238e81c97d00862
ngerrets <nickgerrets@gmail.com>
2021-08-02 10:27:44 +0200

    Modified makefile to conform to the norm.

---
+++ sources.mk
@@ -0,0 +1,13 @@
+SOURCES :=	src/error.c \
+	src/heredoc.c \
+	src/path.c \
+	src/command.c \
+	src/heredoc_file.c \
+	src/str.c \
+	src/str/ft_strnstr.c \
+	src/str/ft_split.c \
+	src/str/ft_strlen.c \
+	src/str/ft_strjoin.c \
+	src/str/ft_substr.c \
+	src/pipex.c \
+	src/main.c \
